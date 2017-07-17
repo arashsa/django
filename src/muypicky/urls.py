2a0d00f769f8b98a3d1d3f5daf15872eecea4214
Arash Saidi <arash@convertelligence.no>
2017-07-18 00:43:36 +0200

    added class based views

--- src/muypicky/urls.py
+++ src/muypicky/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from restaurants.views import home
+from django.views.generic import TemplateView
+from restaurants.views import HomeView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', home),
+    url(r'^$', HomeView.as_view()),
+    url(r'^contact/$', TemplateView.as_view(template_name='contact.html')),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html'))
 ]
--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,5 +1,26 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView
+
+import random
 
 def home(request):
-    return render(request, 'base.html', {'var': 'Myvariable'})
+    num, num1, num2 = random.randint(0, 100000), random.randint(0, 100000), random.randint(0, 100000)
+    context = {
+        'context': True, 
+        'num': [num, num1, num2]
+    }
+    return render(request, 'home.html', context)
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        num, num1, num2 = random.randint(0, 100000), random.randint(0, 100000), random.randint(0, 100000)
+        context = {
+            'context': True, 
+            'num': [num, num1, num2]
+        }
+        return context
